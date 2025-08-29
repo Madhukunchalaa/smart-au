2b1c745cd4672611cb6019237167aec7fc247925
Madhukunchalaa <madhu@smartsolutionsdigi.com>
2025-08-29 17:27:07 +0530

    curosel in our project section

--- js/plugins.js
+++ js/plugins.js
@@ -23,5 +23,24 @@ $(window).on("load", function() {
 
     $("#preloader").fadeOut();
 
+    // Projects Carousel
+    $('.projects-carousel').owlCarousel({
+        loop: true,
+        margin: 30,
+        nav: true,
+        dots: true,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2
+            }
+        }
+    });
+
 });
 
